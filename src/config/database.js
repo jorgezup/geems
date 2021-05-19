631632b5016f077590a52b0b51c099a61953c445
Jorge Zupirolli <jorgebertolo@gmail.com>
2021-05-19 09:18:33 -0400

    fix: error pg_hba.conf

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,9 @@ module.exports = {
     password: process.env.DATABASE_PASSWORD || 'postgres',
     database: process.env.DATABASE_NAME || 'geems',
     port: process.env.DATABASE_PORT || '5432',
-    ssl: true,
+    ssl: {
+        rejectUnauthorized: false
+    },
     define: {
         timestamps: true,
         underscored: true,
