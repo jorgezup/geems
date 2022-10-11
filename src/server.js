04ef30bae3d10378a77c7b366d3eeb12069865c8
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-10-11 11:18:48 -0400

    fix: configuration static files working

--- src/server.js
+++ src/server.js
@@ -1,50 +1,48 @@
 /* Pacotes a serem utilizados */
-const dotenv = require('dotenv')
-const express = require('express')
-const path = require('path')
-const cors = require('cors')
+const dotenv = require("dotenv");
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 /* Dotenv */
-dotenv.config()
+dotenv.config();
 
 /* Importando a database */
-require('./database')
+require("./database");
 
-const routes = require('./routes/routes')
+const routes = require("./routes/routes");
 /* Arquivo a ser utilizado */
 
-
-const methodOverride = require('method-override')
-const session = require('./config/session')
+const methodOverride = require("method-override");
+const session = require("./config/session");
 
 /* Iniciando o app */
-const app = express()
+const app = express();
 
-/* Cors */ 
-app.use(cors()) 
+/* Cors */
+app.use(cors());
 
 /* Session */
-app.use(session)
+app.use(session);
 app.use((req, res, next) => {
-    res.locals.session = req.session
-    next()
-})
-
-
-
+  res.locals.session = req.session;
+  next();
+});
 
 /* View */
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 /* Formulário */
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 /* Pasta public */
-app.use(express.static('public'))
-app.use(methodOverride('_method')) /* sobrescrever o método, para utilizar o PUT (precisa instalar npm install method-override) */
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(
+  methodOverride("_method")
+); /* sobrescrever o método, para utilizar o PUT (precisa instalar npm install method-override) */
 
 /* Caminho das rotas */
-app.use(routes)
+app.use(routes);
 
 /* Iniciar o servidor na porta  */
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
